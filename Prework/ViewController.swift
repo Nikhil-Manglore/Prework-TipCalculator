7e112e40966021505c693f6fcd02c5a27d0c71a4
Nikhil Manglore <nikhilmanglore9@gmail.com>
2022-01-14 00:31:36 -0500

    Added Files From XCode

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Prework
 //
-//  Created by Nikhil Manglore on 1/13/22.
+//  Created by Nikhil Manglore on 1/12/22.
 //  Copyright © 2022 Nikhil Manglore. All rights reserved.
 //
 
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
+        billAmountTextField.becomeFirstResponder()
+        
+        // Set the keypad to only show numbers
+        billAmountTextField.keyboardType = .asciiCapableNumberPad
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Get Total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
